46e010f554b5b26050be8f6b7c20db4f55137b5c
Furkan YÜCEL <81173127+furkycl@users.noreply.github.com>
2025-08-16 16:56:48 +0300

    vercel

--- src/app/projeler/[slug]/page.tsx
+++ src/app/projeler/[slug]/page.tsx
@@ -1,28 +1,12 @@
-import { client } from "@/sanity/lib/client";
-import { singleProjectQuery } from "@/sanity/lib/queries";
-import { notFound } from "next/navigation";
-import ProjectDetailClient from "./ProjectDetailClient";
-import { Project } from "../../../../types/";
-
-export async function generateStaticParams() {
-  const slugs: string[] = await client.fetch(
-    `*[_type == "project" && defined(slug.current)][].slug.current`
-  );
-  return slugs.map((slug) => ({ slug }));
-}
-
-export default async function ProjectDetailPage({
+export default function ProjectDetailPage({
   params,
 }: {
   params: { slug: string };
 }) {
-  const project: Project = await client.fetch(singleProjectQuery, {
-    slug: params.slug,
-  });
-
-  if (!project) {
-    notFound();
-  }
-
-  return <ProjectDetailClient project={project} />;
+  return (
+    <div className="pt-40 text-center">
+      <h1 className="text-4xl">Test Sayfası</h1>
+      <p>Slug: {params.slug}</p>
+    </div>
+  );
 }
