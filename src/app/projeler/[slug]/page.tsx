57b4de3e560d87ae68446c1b85a24803b8856c97
Furkan YÜCEL <81173127+furkycl@users.noreply.github.com>
2025-08-16 14:30:56 +0300

    fix: vercel deploy

--- src/app/projeler/[slug]/page.tsx
+++ src/app/projeler/[slug]/page.tsx
@@ -4,12 +4,20 @@ import { notFound } from "next/navigation";
 import ProjectDetailClient from "./ProjectDetailClient";
 import { Project } from "../../../../types";
 
-type Props = { params: { slug: string } };
+interface PageProps {
+  params: {
+    slug: string;
+  };
+}
 
-export default async function ProjectDetailPage({ params }: Props) {
+export default async function ProjectDetailPage({ params }: PageProps) {
   const project: Project = await client.fetch(singleProjectQuery, {
     slug: params.slug,
   });
-  if (!project) notFound();
+
+  if (!project) {
+    notFound();
+  }
+
   return <ProjectDetailClient project={project} />;
 }
