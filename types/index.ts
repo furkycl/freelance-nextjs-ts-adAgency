f26e9535ab499c79e10782b286738a5902fc3890
Furkan YÜCEL <81173127+furkycl@users.noreply.github.com>
2025-08-17 17:20:11 +0300

    type of slug changed in index.ts

--- types/index.ts
+++ types/index.ts
@@ -1,10 +1,12 @@
 export interface Project {
   _id: string;
   title: string;
-  slug: string;
   category: string;
   imageUrl: string;
   videoUrl: string;
   description: string;
   gallery: string[];
+  slug: {
+    current: string;
+  };
 }
